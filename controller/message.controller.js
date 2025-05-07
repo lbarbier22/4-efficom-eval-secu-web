991051bb27169040d594348507d497bd5d0e55d6
lbarbier22 <barbier.lucas08@gmail.com>
2025-05-07 11:54:02 +0200

    fix : add verify to ensure you can only UD your own messages

--- controller/message.controller.js
+++ controller/message.controller.js
@@ -1,4 +1,6 @@
-const Message = require('../model/role.schema.js');
+const Message = require('../model/message.schema.schema.js');
+const User = require('../model/user.schema.js');
+
 
 const getAll = (req, res, next) => {
     let result = Role.findAll();
@@ -26,14 +28,32 @@ const create = async (req, res, next) => {
     }
 }
 
-const update = (req, res, next) => {
+const update = async (req, res, next) => {
+    verifyIfOwnMessage(req, res, next);
     let result = Message.updateOne(req.body, { id: req.params.id });
     res.status(201).json(result);
 }
 
 const remove = (req, res, next) => {
+    verifyIfOwnMessage(req, res, next);
     let result = Message.remove(req.params.id);
     res.status(200).json(result);
+}
+
+const verifyIfOwnMessage = async (req, res, next) => {
+    const message = await Message.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [User]
+    });
+    if (!message) {
+        return res.status(404).json({ error: "Message not found" });
+    }
+    if (message.userId !== req.user.id) {
+        return res.status(403).json({ error: "You are not the owner of this message" });
+    }
+    next();
 }
 
 module.exports = { getAll, create, getById, update, remove };
